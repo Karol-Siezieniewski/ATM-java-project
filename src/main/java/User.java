c2fced5ae005f1d9451199e43324ed76d203c206
Tomrusz <49609813+Tomrusz@users.noreply.github.com>
2023-01-22 17:21:36 +0100

    Add documentation to user java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -14,7 +14,13 @@ public class User {
     private String hashedPassword;
     private ArrayList<Account> accountsList;
 
-
+    /**
+     * User object constructor
+     * @param name User name
+     * @param lastName User last name
+     * @param password password used for
+     * @param bank Current users bank
+     */
     public User(String name, String lastName, String password, Bank bank) {
         this.name = name;
         this.lastName = lastName;
@@ -24,15 +30,27 @@ public class User {
 
         System.out.printf("New user (%s %s) was created with ID %d. \n", name, lastName, this.id);
     }
-
+    
+    /**
+     * Validation of password input using Bcrypt
+     * @param passwordToValidate Input password used for validation
+     * @return boolean true if password is correct false if incorrect
+     */
     public boolean validatePassword(String passwordToValidate) {
         return BCrypt.checkpw(passwordToValidate, hashedPassword);
     }
 
+    /**
+     * Add account to Users account list
+     * @param account account used to add account to list
+     */
     public void addAccount(Account account) {
         this.accountsList.add(account);
     }
-
+    
+    /**
+     * Display information about all accounts of User
+     */
     public void displayAccountsInfo() {
         System.out.printf("\nSummary for accounts of %s %s\n", this.name, this.lastName);
         for (Account account : accountsList) {
@@ -40,10 +58,4 @@ public class User {
         }
         System.out.println();
     }
-
-    private byte[] generatePassword(String password) throws NoSuchAlgorithmException {
-        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-        return messageDigest.digest(password.getBytes());
-    }
-
 }
